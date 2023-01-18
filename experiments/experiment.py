3203cea7bf416a842ac46e2bf5d2ae8d74c46a55
Caio Guirado <caioguirado@yahoo.com.br>
2023-01-18 18:00:19 +0100

    Structure experiments module and abstract class

--- experiments/experiment.py
+++ experiments/experiment.py
@@ -1,33 +1,21 @@
 from abc import ABC, abstractclassmethod
 
 class Experiment(ABC):
-    pass
 
-from models.uplift_modeling.direct_approach import CausalTree
-from models.regression.problem_transformation.single_target import SingleTarget
-from models.regression.problem_transformation.multi_target import MultiTarget
-from models.regression.problem_transformation.regressor_chains import RegressorChain
-from data.sampler import Sampler
+    @abstractclassmethod
+    def load_config(self):
+        pass
 
-sampler = Sampler()
-data = sampler.sample()
-print(f"data info: {data['X'].shape, data['W'].shape, data['Y_obs'].shape}")
+    @abstractclassmethod
+    def save_results(self):
+        pass
 
-# st_model = SingleTarget(base_estimator=CausalTree, base_estimator_kwargs={})
-# st_model.fit(X=data["X"], w=data["W"], Y=data["Y_obs"])
-# print(st_model.predict(X=data["X"]).shape)
-
-# mt_model = MultiTarget(base_estimator=CausalTree, base_estimator_kwargs={})
-# mt_model.fit(X=data["X"], w=data["W"], Y=data["Y_obs"])
-# print(mt_model.predict(X=data["X"]).shape)
-
-rc_model = RegressorChain(base_estimator=CausalTree, base_estimator_kwargs={})
-rc_model.fit(X=data["X"], w=data["W"], Y=data["Y_obs"])
-print(rc_model.predict(X=data["X"]).shape)
-
-# experiment
-    # experiment type (mainly 2 - regression and rank)
-    # load parameters
-    # train models
-    # evaluate
-    # save/cache results
+    @abstractclassmethod
+    def run(self):
+        # experiment
+            # experiment type (mainly 2 - regression and rank)
+            # load parameters
+            # train models
+            # evaluate
+            # save/cache results
+        pass
